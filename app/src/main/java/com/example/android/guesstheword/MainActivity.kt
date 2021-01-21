1a80d3e339f4dfbcbd964f83f6950ad936200279
abubaker <mohammad_abubaker@yahoo.com>
2021-01-21 15:43:00 +0500

    Starting now...

--- app/src/main/java/com/example/android/guesstheword/MainActivity.kt
+++ app/src/main/java/com/example/android/guesstheword/MainActivity.kt
@@ -24,7 +24,7 @@ import androidx.appcompat.app.AppCompatActivity
  */
 class MainActivity : AppCompatActivity() {
 
-    // onCreate()
+    // Starter code.
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
