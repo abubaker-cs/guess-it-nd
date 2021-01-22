5140c066d0947f20a3bf976d5399b85076ece349
abubaker <mohammad_abubaker@yahoo.com>
2021-01-22 21:40:08 +0500

    Added ViewModeFactory

---
+++ app/src/main/java/com/example/android/guesstheword/screens/score/ScoreViewModel.kt
@@ -0,0 +1,17 @@
+package com.example.android.guesstheword.screens.score
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+
+class ScoreViewModel(finalScore: Int) : ViewModel() {
+
+    // Initialization
+    init {
+        Log.i("ScoreViewModel", "Final score is $finalScore")
+    }
+
+    //
+
+
+
+}
